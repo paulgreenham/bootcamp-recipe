0e1d6e3160eb8f0bfbd48053b56a0670309bc5df
Paul Greenham <paul.greenham@gmail.com>
2019-04-04 13:34:04 +0300

    API results now filtered for erroneous positives by ingredient selection - problem of too few results

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,18 @@ const express = require('express')
 const request = require('request')
 const router = express.Router()
 
+const checkIngredients = function (inputArray, expected) {
+    for(let item of expected)
+        if(!inputArray.includes(item)) {
+            return false
+        }
+    return true
+}
+
+const filterByIngredients = function (recipes, ingredients) {
+    let iList = ingredients.split(",")
+    return recipes.filter(r => checkIngredients(r.ingredients, iList))
+}
 
 router.get("/sanity", function (req, res) {
     res.send("OK")
@@ -13,12 +25,19 @@ router.get("/recipes/:food", function (req, res) {
     }
     request(`http://www.recipepuppy.com/api/?${ingredientsQuery}q=${req.params.food}`, function (error, data, body) {
         let recipes = JSON.parse(data.body).results
-        let toClient = recipes.map(r => { return {
+        let recipeList = recipes.map(r => { return {
             title: r.title,
             link: r.href,
             thumbnail: r.thumbnail,
             ingredients: r.ingredients.split(", ")
         }})
+        let toClient
+        if(req.query.ingredientList) {
+            toClient = filterByIngredients(recipeList, req.query.ingredientList)
+        }
+        else {
+            toClient = recipeList
+        }
         console.log(toClient)
         res.send(toClient)
     })
